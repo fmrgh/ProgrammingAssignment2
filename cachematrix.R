1013025ecfe9539191ffe87b8993d423af0e2149
fmrgh <99422327+fmrgh@users.noreply.github.com>
2022-02-14 09:31:25 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this script consists of two functions which make a matrix, 
+## calculate its inverse, and cache it
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+## the first function, makeCacheMatrix sets the qubic matrix and define functions to calculate its inverse
+## and save the output in a list
+makeCacheMatrix <- function(x = matrix(... ,2, 2)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## the second function "cacheSolve" scopes for the inverse of the setted matrix and turn it if available 
+## if not it calculate the inverse normally, this function should take the output list from "makeCacheMatrix" function as input
+cacheSolve <- function(x) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(matrix(data, 2, 2))
+  x$setsolve(s)
+  s
 }
